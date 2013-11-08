90f49ba903a175821b30bbc8f537a13d074181c6
Adam Wong <adamwong246@gmail.com>
2013-11-08 12:02:13 -0800

    updatE

--- lib/image_cruncher.rb
+++ lib/image_cruncher.rb
@@ -6,7 +6,27 @@ class ImageCruncher
   def self.crunch(src, number_of_colors )
     Magick::ImageList.new(src)
     .quantize(number_of_colors, Magick::RGBColorspace)
-    .color_histogram.sort {|a, b| b[1] <=> a[1]}     
+    .color_histogram
+    .map{|a| 
+      {
+        color: a[0],
+        predominance: a[1]
+      }    
+    }
+    .sort {|a, b| 
+      b[:predominance] <=> a[:predominance]
+    }
+
   end
+
+  def self.crunch_pretty(src, number_of_colors)
+    crunch(src, number_of_colors).map {|a| 
+      {
+        color: a[:color].to_color(Magick::AllCompliance, false, 8, true), 
+        predominance: a[:predominance]
+      } 
+    }
+  end
+
 
 end
