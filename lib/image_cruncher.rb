256bc92f6e54af4e9e3987404cabc3d9959a53fc
Adam Wong <adamwong246@gmail.com>
2013-11-08 12:38:52 -0800

    update

--- lib/image_cruncher.rb
+++ lib/image_cruncher.rb
@@ -1,5 +1,9 @@
 # http://snipplr.com/view/62442/
 
+require 'tilt'
+require 'haml'
+require 'debugger'
+
 class ImageCruncher
   VERSION = "1.0.0"
 
@@ -28,5 +32,24 @@ class ImageCruncher
     }
   end
 
+  def self.generate_profile(src, number_of_colors)    
+    template_file = "../src/profile.html.haml"
+    file_name_output = "#{File.basename(src, ".*" )}_#{File.basename(template_file, ".*" )}"
+    copied_image = File.basename(src)
 
+    template = Tilt.new(template_file)
+    output_string = template.render (
+      {
+        title:        file_name_output,
+        source_image: copied_image,
+        colors:       crunch_pretty(src, number_of_colors)        
+      }
+    )    
+
+    puts "... #{file_name_output}"
+    File.open(file_name_output, 'w') { |f| f.puts(output_string) }
+
+    FileUtils.cp src, copied_image
+                   
+  end
 end
